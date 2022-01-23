d5eeb738378a101a227b4a385641065ed8dfd316
mimshins <mostafa.sh.coderino@gmail.com>
2022-01-23 16:24:49 +0330

    fix template issues

--- templates/cra/default/src/index.js
+++ templates/cra/default/src/index.js
@@ -2,7 +2,6 @@ import App from "./App";
 import CssBaseline from "@sonnat/ui/CssBaseline";
 import SonnatInitializer from "@sonnat/ui/styles/SonnatInitializer";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
 
 ReactDOM.render(
@@ -14,8 +13,3 @@ ReactDOM.render(
   </SonnatInitializer>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
