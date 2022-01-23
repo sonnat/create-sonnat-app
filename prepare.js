f1fadb49fcbab4451a6e5f9954f2fd714b8e697f
mimshins <mostafa.sh.coderino@gmail.com>
2022-01-23 16:32:09 +0330

    update building process

--- prepare.js
+++ prepare.js
@@ -1,9 +1,13 @@
 const cpy = require("cpy");
 const path = require("path");
 
+const dest = path.join(__dirname, "lib");
+
 void (async () => {
-  await cpy("**", path.join(__dirname, "lib", "templates"), {
+  await cpy("**", path.join(dest, "templates"), {
     parents: true,
     cwd: path.join(__dirname, "templates")
   });
+
+  await cpy(["LICENSE", "README.md"], dest);
 })();
